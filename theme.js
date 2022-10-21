6d479c1eedad624dfb790fa1d32402625c896074
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-21 15:57:31 +0900

    로그인 화면-input 컴포넌트 6:54 PropTypes가 인식되지 않는 오류 발생

--- theme.js
+++ theme.js
@@ -2,14 +2,22 @@ const Colors = {
   white: '#ffffff',
   black: '#111111',
   main: '#3f8aec',
+  grey_0: '#d5d5d5',
+  grey_1: '#a6a6a6',
 };
 
 export const theme = {
   background: Colors.white,
-  text: Colors.text,
+  text: Colors.black,
 
   // Button
   btnBackground: Colors.main,
   btnTitle: Colors.white,
   btnTextLink: Colors.main,
+
+  // Input
+  inputBackground: Colors.white,
+  inputLabel: Colors.grey_1,
+  inputPlaceholder: Colors.grey_1,
+  inputBorder: Colors.grey_1,
 };
