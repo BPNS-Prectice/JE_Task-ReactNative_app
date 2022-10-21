6759b59d2e69a3f5cbd4fabf6ea6da7800c21d01
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-21 15:06:03 +0900

    로그인화면 -Button 컴포넌트(완)

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "styled-components";
 import styled from "styled-components";
-import { Button } from "react-native";
+import { Button } from "../components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+// import { theme } from "../../theme";
 
 const Container = styled.View`
   flex: 1;
@@ -20,12 +22,19 @@ const StyledText = styled.Text`
 
 export default function Signin({ navigation }) {
   const insets = useSafeAreaInsets();
-   // 정확힌 모르겠으나 헤더 대신 패딩을 넣는 ? 그런 용도인듯
+  // 정확힌 모르겠으나 헤더 대신 패딩을 넣는 ? 그런 용도인듯
+  const theme = useContext(ThemeContext);
 
   return (
     <Container insets={insets}>
       <StyledText>Signin</StyledText>
-      <Button title="signup" onPress={() => navigation.navigate('Signup')} />
+      <Button title="Sign up" onPress={() => console.log("signin")} />
+      <Button
+        title="or sign up"
+        onPress={() => navigation.navigate("Signup")}
+        containerStyle={{ marginTop: 0, backgroundColor: "transparent" }}
+        textStyle={{ color: theme.btnTextLink, fontSize: 18 }}
+      />
     </Container>
   );
 }
