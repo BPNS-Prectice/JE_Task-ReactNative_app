af4f38416e8ae81360e801b428d0d1ef19099975
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-24 17:13:13 +0900

    회원가입 화면 6:30 KeyboardAwareScrollView import 시 오류 발생

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
-import { Button } from 'react-native';
+import { Button, Input } from "../components";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+// import { FlatList } from 'react-native'
+// import listenToKeyboardEvents from './KeyboardAwareHOC'
 
 const Container = styled.View`
   flex: 1;
@@ -8,18 +13,70 @@ const Container = styled.View`
   align-items: center;
   background-color: ${({ theme }) => theme.background};
   padding: 0 20px;
+  padding-top: ${({ insets: { top } }) => top}px;
+  padding-bottom: ${({ insets: { bottom } }) => bottom}px;
 `;
 
-const StyledText = styled.Text`
-  font-size: 30px;
-  color: #111111;
-`;
+export default function Signup() {
+  const insets = useSafeAreaInsets();
+  // 정확힌 모르겠으나 헤더 대신 패딩을 넣는 ? 그런 용도인듯
 
-export default function Signup({ navegation }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const refEmail = useRef(null);
+  const refPassword = useRef(null);
+  const refPasswordConfirm = useRef(null);
+
+  const _handleSignupBtnPress = () => {
+    console.log('signup')
+  }
+
   return (
-    <Container>
-      <StyledText>Signup</StyledText>
-      {/* <Button title="signup" onPress={() => navegation.navigate('Signup')} /> */}
+    <KeyboardAwareScrollView>
+    <Container insets={insets}>
+      <Input
+        label="Name"
+        placeholder="Name"
+        returnkeyType="next"
+        value={name}
+        onChangeText={setName}
+        onSubmitEditing={() => refEmail.current.focus()}
+      />
+      <Input
+        ref={refEmail}
+        label="Email"
+        placeholder="Email"
+        returnkeyType="next"
+        value={email}
+        onChangeText={setEmail}
+        onSubmitEditing={() => refPassword.current.focus()}
+      />
+      <Input
+        ref={refPassword}
+        label="Password"
+        placeholder="Password"
+        returnkeyType="next"
+        value={password}
+        onChangeText={setPassword}
+        isPassword={true} // 비밀번호 입력 시 특수문자로 노출
+        onSubmitEditing={() => refPasswordConfirm.current.focus()}
+      />
+      <Input
+        ref={refPasswordConfirm}
+        label="Password 확인"
+        placeholder="Password 확인"
+        returnkeyType="done"
+        value={passwordConfirm}
+        onChangeText={setPasswordConfirm}
+        isPassword={true} // 비밀번호 입력 시 특수문자로 노출
+        onSubmitEditing={_handleSignupBtnPress}
+      />
+      <Button title="Sign in" onPress={_handleSignupBtnPress} />
+      
     </Container>
+    </KeyboardAwareScrollView>
   );
 }
